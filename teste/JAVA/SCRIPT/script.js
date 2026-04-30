5884f49d6d4cd1fecb805a7328e752a4928319a3
lucianodinalli48-spec <lucianodinalli48@gmail.com>
2026-04-29 22:19:16 -0300

    29-4-2026

--- teste/JAVA/SCRIPT/script.js
+++ teste/JAVA/SCRIPT/script.js
@@ -9,11 +9,12 @@ function adicionarMensagem() {
 
 function removerTarefa() {
     //Variavel para capturar o valor do input
-    let input = document.getElementById("Tarefa"); 
+    let input = document.getElementById("mensagem"); 
     
     //Fixar o valor do input
     let mensagem = input.value
     document.getElementById("mensagem").textContent = ""
+    document.getElementById("Tarefa").value = "";
 }
 
 
